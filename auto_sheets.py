874fabb63c6fae0ca74e1e236705c6ad0f2a7713
Samuel Odundo <samieodd@gmail.com>
2020-01-02 13:54:49 +0300

    Added comments

--- auto_sheets.py
+++ auto_sheets.py
@@ -1,5 +1,14 @@
+from bs4 import BeautifulSoup
 import requests
 
+# perform a GET request to download the page
 page = requests.get("https://www.google.com/search?q=Yo")
 
+# check if the request was successful
 print(page.status_code)
+
+# parse the page using BeautifulSoup
+soup = BeautifulSoup(page.content, 'html.parser')
+
+# get data using a specific element id
+print(soup.find_all(id="resultStats"))
