37598d0a9500f836be5bc510cfdc6d36b59df5e9
Samuel Odundo <samieodd@gmail.com>
2020-01-02 14:56:37 +0300

    Created a function for the whole process

--- auto_sheets.py
+++ auto_sheets.py
@@ -1,14 +1,31 @@
 from bs4 import BeautifulSoup
 import requests
+from selenium import webdriver
+import time
 
-# perform a GET request to download the page
-page = requests.get("https://www.google.com/search?q=Yo")
+searchTerm = "Ahem"
+url = "https://www.google.com/search?q={0}".format(searchTerm)
+driver = webdriver.Chrome(
+    'C:/ProgramData/chocolatey/lib/chromedriver/tools/chromedriver.exe')
 
-# check if the request was successful
-print(page.status_code)
 
-# parse the page using BeautifulSoup
-soup = BeautifulSoup(page.content, 'html.parser')
+def autoSearch(driver, url):
+    # use driver to perform a GET request
+    # while giving it time to render the page
+    driver.get(url)
+    time.sleep(5)
 
-# get data using a specific element id
-print(soup.find_all(id="resultStats"))
+    # pass it on to BeautifulSoup
+    # parse the page using BeautifulSoup
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    # print(soup.prettify())
+
+    # get data using a specific element id and
+    # assign it to a variable
+    resultStats = soup.find_all(id="resultStats")[0].get_text()
+    return resultStats
+
+
+resultStats = autoSearch(driver, url)
+
+print(searchTerm, ' has ', resultStats)
